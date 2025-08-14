7a9caa739f713c43ffcf8b5d8ae7b985dd14207a
sofy0708 <linsofia07082000@gmail.com>
2025-08-14 20:46:50 +0200

    add circleshape.py (add CircleShape Class)

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+# Base class for game objects, Sprite is a built-in class in pygame 
+# for visible game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"): # returns True if the object has the named attribute
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass

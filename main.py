a6095bf2287653edb6081b04b25023232f4b82b5
sofy0708 <linsofia07082000@gmail.com>
2025-08-14 21:39:27 +0200

    add player.py (add Player class), modify constants.py (add PLAYER_RADIUS), modify main.py (add player design in the game), added a bunch of imports

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     pygame.init
@@ -7,7 +8,9 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)  # Create a player at the center of the screen
 
     clock = pygame.time.Clock() #object that helps tracking time
     dt = 0 # initializing delta time
@@ -18,7 +21,10 @@ def main():
             if event.type == pygame.QUIT: # pygame.QUIT is the event that happens when you click the close button on the window
                 return
 
-        screen.fill("black")  # Or screen.fill((0, 0, 0)) - Fills the screen with black
+        screen.fill("black")  # Or screen.fill((0, 0, 0)) - Fills the screen with black        
+        
+        player.draw(screen)
+        
         pygame.display.flip()  # Update the display
 
         # time tracker/checker at the end of every loop iteration
@@ -27,10 +33,6 @@ def main():
         # "delta time" is the time that passed from one frame to another
         
         
-
-
-
-
 
 if __name__ == "__main__":
     main()

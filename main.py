66e564363c09589169d022c323a29d672bc1705e
sofy0708 <linsofia07082000@gmail.com>
2025-08-19 17:47:05 +0200

    create groups

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)  # Create a player at the center of the screen
     clock = pygame.time.Clock() #object that helps tracking time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     dt = 0 # initializing delta time
 
     while True: #keeps looping infinitely until you tell it to stop
@@ -15,7 +21,7 @@ def main():
                 return
 
         player.update(dt)
-        
+
         screen.fill("black")  # Or screen.fill((0, 0, 0)) - Fills the screen with black        
         player.draw(screen)
         pygame.display.flip()  # Update the display
